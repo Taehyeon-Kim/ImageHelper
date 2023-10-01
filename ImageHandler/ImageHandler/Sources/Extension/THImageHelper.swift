25cf2e82bba0a8237e48bc5cc7921921ba8cdb1b
김태현 <taehyk528@gmail.com>
2023-10-01 15:44:33 +0900

    Rename >>> THImageHelper

--- ImageHandler/ImageHandler/Sources/Extension/THImageHelper.swift
+++ ImageHandler/ImageHandler/Sources/Extension/THImageHelper.swift
@@ -1,5 +1,5 @@
 //
-//  THWrapper.swift
+//  THImageHelper.swift
 //  ImageHandler
 //
 //  Created by taekki on 2023/10/01.
@@ -10,7 +10,7 @@ import Foundation
 /// Generic 사용
 /// - 여러 타입에 호환되게 사용할 수 있도록 함
 /// - Wrapper로 한 번 감싸고 그 안에 있는 base(여러 타입)을 사용해서 메서드에 접근하도록 함
-public struct THWrapper<Base> {
+public struct THImageHelperWrapper<Base> {
   public let base: Base
   
   public init(base: Base) {
@@ -19,12 +19,12 @@ public struct THWrapper<Base> {
 }
 
 /// AnyObject와 NSObject의 차이
-protocol THCompatible: NSObject {}
+protocol THImageHelperCompatible: NSObject {}
 
-extension THCompatible {
+extension THImageHelperCompatible {
   /// 계산 프로퍼티이기 때문에 인스턴스를 매번 생성하지만,
   /// 구조체이므로 불변성을 보장할 수 있다.
-  public var th: THWrapper<Self> {
-    return THWrapper(base: self)
+  public var th: THImageHelperWrapper<Self> {
+    return THImageHelperWrapper(base: self)
   }
 }
--- ImageHandler/ImageHandler/Sources/Extension/UIImageView+THImageHelper.swift
+++ ImageHandler/ImageHandler/Sources/Extension/UIImageView+THImageHelper.swift
@@ -1,5 +1,5 @@
 //
-//  UIImageView+THWrapper.swift
+//  UIImageView+THImageHelper.swift
 //  ImageHandler
 //
 //  Created by taekki on 2023/10/01.
@@ -9,12 +9,11 @@ import UIKit
 
 /// 사용할 클래스에 채택
 typealias THImageView = UIImageView
-extension THImageView: THCompatible {}
+extension THImageView: THImageHelperCompatible {}
 
 /// 사용할 메서드 선언
-extension THWrapper where Base: UIImageView {
-
-  func setImage(with url: String) {
+extension THImageHelperWrapper where Base: UIImageView {
+  public func setImage(with url: String) {
     Task {
       do {
         let imageData = try? await ImageHelper.shared.fetch(with: url)
